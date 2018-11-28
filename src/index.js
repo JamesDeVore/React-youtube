ef0858e6931f57cd2145c30b36253e5d0db777cb
James DeVore <jdevore4592@gmail.com>
2018-11-28 10:32:28 -0500

    API search successful

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyCDHxEwzplSqxvsfDulPcBW9pM-Dis1N7E'
+const axios = require('axios');
+
+class App extends Component {
+
+  constructor (){
+    super()
+
+    this.state = {
+      videos:[],
+      selectedVideo:null
+    }
+  }
+
+  const videoSearch = (term) => {
+    const url = 'https://www.googleapis.com/youtube/v3/search'
+    const params = {
+      part: 'snippet',
+      key: API_KEY,
+      q: term,
+      type: 'video'
+    };
+
+    axios.get(url, {params})
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error=>console.error(error))
+  }
+
+  return (
+    <SearchBar onSearchTermChange={videoSearch} />
+  )
+}
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
